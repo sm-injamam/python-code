555e3dc1e35b741c46171e5640e8ad3deb865e39
Injamam Ul Haque Apu <82649969+sm-injamam@users.noreply.github.com>
2021-04-20 14:41:41 +0600

    added break and continue command.
    the concept for easy undertand.

---
+++ break&continue.py
@@ -0,0 +1,13 @@
+
+while true:
+    n= int(input("Enter a number(press 0 to exit) : "))
+    if n==0:
+        break
+    if n<0:
+        print("Please enter a positive number")
+        continue
+    print ('Square is', n*n)
+
+
+
+
